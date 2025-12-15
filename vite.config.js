203742ec35e20bbbc273c9d53e5de9ca9b383779
Joshua-Raphael <joshuaraphael.gayoba@hcdc.edu.ph>
2025-12-15 13:44:00 +0800

    ..

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/joshua-portfolio",
+  base: "/joshua-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
